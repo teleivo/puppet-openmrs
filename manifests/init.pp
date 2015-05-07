275da9587aaa24afb0a2ecab0570ecd672c93d0f
teleivo <mrteleivo@gmail.com>
2015-05-07 15:33:01 +0200

    exchange tomcat class declaration with include
    just establishes the dependency, the data should be included on higher level
    via hiera

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,36 +1,20 @@
 class openmrs197 (
-    $version = '6.0.29',
     $tomcat_user = 'tomcat6',
     $tomcat_user_home = '/opt/tomcat6',
-    $tomcat_http_port = '8080',
-    $tomcat_java_opts = undef,
+    $tomcat_module_deployment_path = '/opt/tomcat6/apache-tomcat-6.0.29/webapps',
     $db_name = 'openmrs',
     $db_owner = 'openmrs',
     $db_owner_password = 'openmrs',
 ) {
-
-    $tomcat_module_deployment_path = "${tomcat_user_home}/apache-tomcat-6.0.29/webapps"
+    include tomcat6
 
     package { "openjdk-7-jdk":
         ensure => "present",
-    }->
+    }
 
     package { "libaio1":
         ensure => "present",
-    }->
-
-    class { 'tomcat6':
-        user         => $tomcat_user,
-        user_home    => $tomcat_user_home,
-        http_port    => $tomcat_http_port,
-        tomcat_users => [
-                            {   name     => "admin",
-                                password => "admin",
-                                roles    => 'tomcat,admin,manager,manager-gui'
-                            },
-                        ],
-        java_opts   => $tomcat_java_opts,
-    }->
+    }
 
     class { 'openmrs197::staging':
         user                    => $tomcat_user,
