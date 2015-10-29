b6392e9281fc29284885bf6daa3a70efc84feb2e
teleivo <mrteleivo@gmail.com>
2015-10-29 16:55:07 +0100

    remove openmrs-runtime.properties resource
    * the file is created once the openmrs install
     wizard is run. creating it somehow messes with the
     database credentials and installation fails during
     update of database schema

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,43 +7,20 @@ class openmrs (
   $db_owner = 'openmrs',
   $db_owner_password = 'openmrs',
 ) {
-  $openmrs_runtime_properties_filename = 'openmrs-runtime.properties'
-  $openmrs_runtime_properties_path =
-  "${tomcat_catalina_base}/${openmrs_runtime_properties_filename}"
-  
-  $openmrs_runtime_properties = {
-    'connection.url'         =>
-    "jdbc:mysql://${db_host}:3306/${db_name}?autoReconnect=true&sessionVariables=storage_engine=InnoDB&useUnicode=true&characterEncoding=UTF-8",
-    'connection.username'        => $db_owner,
-    'connection.password'        => $db_owner_password,
-    'auto_update_database'       => false,
-    'module.allow_web_admin'     => true,
-    'application_data_directory' => $openmrs_application_data_directory,
-  }
-
   include 'tomcat'
 
-  tomcat::war { 'openmrs.war':
-    catalina_base => $tomcat_catalina_base,
-    war_source    =>
-    'http://sourceforge.net/projects/openmrs/files/releases/OpenMRS_Platform_1.11.4/openmrs.war',
-    require       => [ File[$openmrs_runtime_properties_path],
-                      File[$openmrs_application_data_directory]
-    ]
-  }
-  file { $openmrs_runtime_properties_path:
-    ensure  => present,
-    owner   => $tomcat_user,
-    group   => $tomcat_user,
-    mode    => '0644',
-    content =>
-      template("openmrs/${openmrs_runtime_properties_filename}.erb"),
-  }
   file { $openmrs_application_data_directory:
     ensure => 'directory',
     owner  => $tomcat_user,
     group  => $tomcat_user,
     mode   => '0755',
+  }
+
+  tomcat::war { 'openmrs.war':
+    catalina_base => $tomcat_catalina_base,
+    war_source    =>
+    'http://sourceforge.net/projects/openmrs/files/releases/OpenMRS_Platform_1.11.4/openmrs.war',
+    require       => File[$openmrs_application_data_directory],
   }
 
   mysql::db { $db_name:
--- spec/classes/openmrs_spec.rb
+++ spec/classes/openmrs_spec.rb
@@ -11,28 +11,12 @@ describe 'openmrs', :type => :class do
       it { is_expected.to compile }
       it { is_expected.to contain_class('openmrs') }
       it { is_expected.to contain_class('tomcat') }
-      it { is_expected.to contain_file('/var/lib/tomcat7/openmrs-runtime.properties')
-            .with(
-              'ensure' => 'present',
-              'owner'  => 'tomcat7',
-              'group'  => 'tomcat7',
-              'mode'   => '0644',
-            )
-            .with_content(/connection.url=jdbc:mysql:\/\/localhost:3306\/openmrs\?autoReconnect=true&sessionVariables=storage_engine=InnoDB&useUnicode=true&characterEncoding=UTF-8/)
-            .with_content(/connection.username=openmrs/)
-            .with_content(/connection.password=openmrs/)
-            .with_content(/auto_update_database=false/)
-            .with_content(/module.allow_web_admin=true/)
-            .with_content(/application_data_directory=\/opt\/openmrs/)
-      }
-      it {
-        is_expected.to contain_file('/opt/openmrs').with(
+      it { is_expected.to contain_file('/opt/openmrs').with(
           'ensure' => 'directory',
           'owner'  => 'tomcat7',
           'group'  => 'tomcat7',
           'mode'   => '0755'
-        )
-      }
+      ) }
       it { is_expected.to contain_tomcat__war('openmrs.war').with(
         'war_source' => 'http://sourceforge.net/projects/openmrs/files/releases/OpenMRS_Platform_1.11.4/openmrs.war',
       ) }
@@ -54,14 +38,6 @@ describe 'openmrs', :type => :class do
         :db_owner_password => $db_owner_password,
       } }
 
-      it { is_expected.to contain_file('/var/lib/tomcat7/openmrs-runtime.properties')
-            .with_content(/connection.url=jdbc:mysql:\/\/db.example.com:3306\/medical\?autoReconnect=true&sessionVariables=storage_engine=InnoDB&useUnicode=true&characterEncoding=UTF-8/)
-            .with_content(/connection.username=medicaladmin/)
-            .with_content(/connection.password=admin123/)
-            .with_content(/auto_update_database=false/)
-            .with_content(/module.allow_web_admin=true/)
-            .with_content(/application_data_directory=\/opt\/openmrs/)
-      }
       it { is_expected.to contain_mysql_database($db_name) }
       it { is_expected.to contain_mysql_user($db_owner + "@" + $db_host) }
     end
@@ -74,23 +50,12 @@ describe 'openmrs', :type => :class do
         :openmrs_application_data_directory => $openmrs_application_data_directory,
       } }
 
-      it { is_expected.to contain_file('/var/lib/tomcat7/openmrs-runtime.properties')
-            .with(
-              'ensure' => 'present',
-              'owner'  => 'tomcat7',
-              'group'  => 'tomcat7',
-              'mode'   => '0644',
-            )
-            .with_content(/application_data_directory=\/var\/lib\/openmrs/)
-      }
-      it {
-        is_expected.to contain_file($openmrs_application_data_directory).with(
+      it { is_expected.to contain_file($openmrs_application_data_directory).with(
           'ensure' => 'directory',
           'owner'  => 'tomcat7',
           'group'  => 'tomcat7',
           'mode'   => '0755'
-        )
-      }
+      ) }
     end
   end
 end
